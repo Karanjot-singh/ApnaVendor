18e1f81311a1ac1bf59113b141c5585b07bdacde
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-15 01:02:28 +0530

    UI changes

--- lib/components/my_bottom_nav_bar.dart
+++ lib/components/my_bottom_nav_bar.dart
@@ -12,20 +12,24 @@ class MyBottomNavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(
-        left: kDefaultPadding * 2,
-        right: kDefaultPadding * 2,
-        bottom: kDefaultPadding,
+        left: kDefaultPadding * 1.5,
+        right: kDefaultPadding * 1.5,
+        bottom: kDefaultPadding * 0.5,
       ),
-      height: 80,
+      height: 60,
       decoration: BoxDecoration(
-        
+        borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(36),
+                topRight: Radius.circular(36),
+              ),
+             
         color: Colors.white,
         boxShadow: [
-          // BoxShadow(
-          //   offset: Offset(0, -10),
-          //   blurRadius: 35,
-          //   color: kPrimaryColor.withOpacity(0.38),
-          // ),
+          BoxShadow(
+            offset: Offset(0, -10),
+            blurRadius: 35,
+            color: Colors.black.withOpacity(0.13),
+          ),
         ],
       ),
       child: Row(
--- lib/constants.dart
+++ lib/constants.dart
@@ -5,10 +5,6 @@ const kPrimaryColor = Color(0xffb1006a);
 const kAccentColor = Color(0xff00B147);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
-// String values of the above for Customising icon Colours
-// String cPrimaryColor = "0xffb1006a";
-// String cAccentColor = "0xff00B147";
-// String cTextColor = "0xFF3C4046";
-// String cBackgroundColor = "0xFFF9F8FD";
+// For Customising icon Colours
 
 const double kDefaultPadding = 20.0;
