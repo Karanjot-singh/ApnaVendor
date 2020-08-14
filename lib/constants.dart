ce9dc49c345993d715cc5f10996c35e4fa07436a
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-14 20:43:36 +0530

    edits

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 // Colors that we use in our app
-const kPrimaryColor = Color(0xFF0C9869);
+const kPrimaryColor = Color(0xb1006a);
+const kAccentColor = Color(0x00B147);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
 
