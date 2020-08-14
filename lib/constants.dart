20eb1ef91f753009218835c5026e6bbec61ae6de
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-15 00:41:38 +0530

    icon colours adjusted

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,5 +5,10 @@ const kPrimaryColor = Color(0xffb1006a);
 const kAccentColor = Color(0xff00B147);
 const kTextColor = Color(0xFF3C4046);
 const kBackgroundColor = Color(0xFFF9F8FD);
+// String values of the above for Customising icon Colours
+// String cPrimaryColor = "0xffb1006a";
+// String cAccentColor = "0xff00B147";
+// String cTextColor = "0xFF3C4046";
+// String cBackgroundColor = "0xFFF9F8FD";
 
 const double kDefaultPadding = 20.0;
