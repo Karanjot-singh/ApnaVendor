09ca9385992d5275b93bdac1b67ea4ef6fab0a79
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-16 17:03:44 +0530

    edits

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:Ignite/components/my_bottom_nav_bar.dart';
 import 'package:Ignite/screens/home/components/body.dart';
-
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // drawer: NavDrawer() ,
       appBar: buildAppBar(),
       body: Body(),
       bottomNavigationBar: MyBottomNavBar(),
