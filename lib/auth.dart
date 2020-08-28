e80ff505deef4e23c3d9fa655cca442c72372992
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-28 14:37:53 +0530

    Addded Configs for database

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,39 +1,39 @@
-//firebase_login
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-final FirebaseAuth _auth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = GoogleSignIn();
+// //firebase_login
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// final FirebaseAuth _auth = FirebaseAuth.instance;
+// final GoogleSignIn googleSignIn = GoogleSignIn();
 
-//async functions will not be executed asap, won't change or stop others functions
-// that are working on with the application at the moment
+// //async functions will not be executed asap, won't change or stop others functions
+// // that are working on with the application at the moment
 
-Future<User> signInWithGoogle() async {
+// Future<User> signInWithGoogle() async {
 
-  //google related starts
-  // Let the user sign in , get auth from google and get credential > to firebase, authenticate firebase
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+//   //google related starts
+//   // Let the user sign in , get auth from google and get credential > to firebase, authenticate firebase
+//   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+//   final GoogleSignInAuthentication googleSignInAuthentication =
+//       await googleSignInAccount.authentication;
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-      idToken: googleSignInAuthentication.idToken,
-      accessToken: googleSignInAuthentication.accessToken);
-   //google related stuff ends
+//   final AuthCredential credential = GoogleAuthProvider.getCredential(
+//       idToken: googleSignInAuthentication.idToken,
+//       accessToken: googleSignInAuthentication.accessToken);
+//    //google related stuff ends
 
-  //firebase user credntial passes
-  final AuthResult authResult = await _auth.signInWithCredential(credential);
-  final User user = authResult.user; //firebase user
+//   //firebase user credntial passes
+//   final AuthResult authResult = await _auth.signInWithCredential(credential);
+//   final User user = authResult.user; //firebase user
 
-  assert(!user.isAnonymous);
-  assert(await user.getIdToken() != null);
+//   assert(!user.isAnonymous);
+//   assert(await user.getIdToken() != null);
 
-  final User currentUser = await _auth.currentUser();
-  assert(currentUser.uid == user.uid);
-  //firebase signed in user is the same as the google sign in user
+//   final User currentUser = await _auth.currentUser();
+//   assert(currentUser.uid == user.uid);
+//   //firebase signed in user is the same as the google sign in user
 
-  return user;
-}
+//   return user;
+// }
 
-void signOutGoogle() async {
-  await googleSignIn.signOut();
-}
+// void signOutGoogle() async {
+//   await googleSignIn.signOut();
+// }
